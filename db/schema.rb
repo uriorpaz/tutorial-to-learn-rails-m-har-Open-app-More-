d62f76b0c8aaef19e29d6eb43c2afefcf4ff34ad
uri orpaz <uriorpaz@uris-Mac-mini.local>
2017-05-31 16:02:58 +0200

    chapter 8

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,20 @@ ActiveRecord::Schema.define(version: 20170510221549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
+  create_table "microposts", id: :bigserial, force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+  create_table "users", id: :bigserial, force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
